a8aa7880de2f34836c2083e29ddaf5a47210178c
AmgalanA <amgalan12346@gmail.com>
2023-12-31 12:41:18 +0300

    first commit

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+
+    </div>
+  )
+}
