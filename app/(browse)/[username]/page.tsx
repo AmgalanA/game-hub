d515fd9ce5bfacad25749db2c670eb857c597f0c
AmgalanA <amgalan12346@gmail.com>
2024-01-01 02:33:32 +0300

    second commit

---
+++ app/(browse)/[username]/page.tsx
@@ -0,0 +1,13 @@
+interface UserPageProps {
+    params: {
+        username: string;
+    }
+}
+
+const UserPage = ({ params }: UserPageProps) => {
+  return (
+    <div>{params.username}</div>
+  )
+}
+
+export default UserPage
---
+++ lib/recommended-service.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export const getRecommended = async () => {
+    // await new Promise(resolve => {
+        // return setTimeout(resolve, 5000)
+    // })
+
+    let userId;
+
+    try {
+        const self = await getSelf();
+
+        userId = self.id
+    } catch (error) {
+        userId = null;   
+    }
+
+    let users = [];
+
+    if (userId) {
+        users = await db.user.findMany({
+            where: {
+                NOT: {
+                    id: userId
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    } else {
+        users = await db.user.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    }
+
+
+    return users;
+}
